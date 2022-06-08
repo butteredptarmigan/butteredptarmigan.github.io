363afbe0d90f9fd06fd309ca97d142ae570d702b
Daniel Hirsch <bloohirsch@gmail.com>
2022-06-08 22:15:16 +0200

    Introduce DatewiseStorage component for storing global datasets with timestamps

---
+++ src/storage/Storage.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+export type StorageObject = { [key: number]: number; };
+
+export type StorageOperator = {
+    items: Storage;
+    add: Function;
+    remove: Function;
+    includes: Function;
+    toggle: Function;
+};
+
+const DatewiseStorage = ($: any) => {
+    let [storage, setStorage]: [StorageObject, Function] = useState({});
+
+    const add = (id: number | string) => {
+        const item = Number(id);
+
+        if (item in storage) {
+            return;
+        }
+
+        const date = Date.now();
+        setStorage({
+            ...storage,
+            [item]: date
+        });
+    };
+
+    const remove = (id: number | string) => {
+        const item = Number(id);
+
+        if (item in storage) {
+            const { [item]: removed, ...updatedStorage } = storage;
+            setStorage(updatedStorage);
+        }
+    };
+
+    const includes = (id: number | string) => {
+        const item = Number(id);
+        return item in storage;
+    };
+
+    const toggle = (id: number | string) => {
+        const item = Number(id);
+        includes(item)
+            ? remove(item)
+            : add(item);
+    };
+
+    const operator = {
+        items: Object.seal(storage),
+        add,
+        remove,
+        includes,
+        toggle
+    };
+
+    if (typeof $.children != 'function') {
+        throw new TypeError('The only child of DatewiseStorage must be a function');
+    }
+
+    return $.children(operator);
+};
+
+export default DatewiseStorage;
