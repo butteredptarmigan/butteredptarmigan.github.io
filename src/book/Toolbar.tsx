8704a7de56fe4a42b9ba1e756a1a1f439db77edc
Daniel Hirsch <bloohirsch@gmail.com>
2022-06-09 00:25:29 +0200

    Add book toolbar component

---
+++ src/book/Toolbar.tsx
@@ -0,0 +1,10 @@
+import I from '../ui/Icon';
+
+const Toolbar = () => (
+    <div>
+        <button><I>favorite</I></button>
+        <button><I>import_contacts</I></button>
+    </div>
+);
+
+export default Toolbar;
