7689059393a3041cb2c7725fd1b92d38ec8fc7d0
Daniel Hirsch <bloohirsch@gmail.com>
2022-06-10 04:25:42 +0200

    Style show favorites button

--- src/explore/ShowFavorites.tsx
+++ src/explore/ShowFavorites.tsx
@@ -1,6 +1,7 @@
 import { NavLink, useMatch } from 'react-router-dom';
 import I from '../ui/Icon';
 import classNames from 'classnames';
+import './ShowFavorites.scss';
 
 export const ShowFavorites = () => {
     const active = useMatch('/explore/favorites');
