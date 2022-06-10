f6b5d59ec3f37a5d6812543f818eb9e1a565034e
Daniel Hirsch <bloohirsch@gmail.com>
2022-06-10 04:05:31 +0200

    Implement proper navigation between the explore tab and favorite books

--- src/explore/Explore.tsx
+++ src/explore/Explore.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
-import { Book as BookInterface } from '../api/types';
+import { useState } from 'react';
 import { StorageOperator } from '../storage/Storage';
 import { Fetcher } from './Fetcher';
-import { Link, Outlet, useOutletContext } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import Tab from '../Tab';
 import { SearchInput } from './SearchInput';
-import I from '../ui/Icon';
 import './Explore.scss';
-
-const ShowFavorites = () => (
-    <Link to='/explore/favorites' className='ShowFavorites'>
-        <I>favorite</I>
-    </Link>
-);
+import { ShowFavorites } from './ShowFavorites';
 
 const Explore = () => {
     const [fetcher] = useState(() => new Fetcher('book'));
---
+++ src/explore/ShowFavorites.tsx
@@ -0,0 +1,18 @@
+import { NavLink, useMatch } from 'react-router-dom';
+import I from '../ui/Icon';
+import classNames from 'classnames';
+
+export const ShowFavorites = () => {
+    const active = useMatch('/explore/favorites');
+
+    return (
+        <NavLink
+            to={active ? '/explore' : '/explore/favorites'}
+            className='ShowFavorites'
+        >
+            <I className={classNames('Icon', { 'Icon-filled': active })}>
+                favorite
+            </I>
+        </NavLink>
+    );
+};
